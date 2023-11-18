1bf9d5c5a49b5d0423bb3ce469d7e7a44e9688af
KeithLin724 <keithlin724@gmail.com>
2023-11-19 00:29:19 +0800

    update the cut prompt function

--- Book_To_Comics_Client/backend/ai_router.py
+++ Book_To_Comics_Client/backend/ai_router.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+import httpx
+import ast
+
 from router_item import CutPromptItem, ListPromptItem
 
 ai_router = APIRouter()
@@ -6,7 +9,38 @@ ai_router = APIRouter()
 
 @ai_router.post("/cut_prompt")
 async def cut_prompt(message: CutPromptItem):
-    return {"message": "not ready"}
+    # TODO: "cut prompt" prompt function
+    CUT_PROMPT_FUNC = (
+        lambda message: f"can you cut list of prompt in the message to describe the image how to look like, return like ['...' , '...' , ...], message is {message}"
+    )
+
+    json_data = {
+        "type_service": "chat",
+        "prompt": CUT_PROMPT_FUNC(message=message),
+    }
+
+    # TODO: send request to server
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            "http://140.113.238.35:5000/generate_service",
+            json=json_data,
+            timeout=10,
+        )
+
+    # TODO: get the result
+    result = response.json()
+
+    provider, message = result["provider"], result["message"]
+
+    message = message.replace("\n", "")
+
+    prompt_list = ast.literal_eval(message)
+
+    return {
+        "state": "success",
+        "provider": provider,
+        "prompt_list": prompt_list,
+    }
 
 
 @ai_router.post("/list_prompt_to_image")

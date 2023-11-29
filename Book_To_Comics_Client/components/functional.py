d47fcff25a2ec2a2df5e0c038dc72962778bc7ee
KeithLin724 <keithlin724@gmail.com>
2023-11-29 17:41:51 +0800

    put copy and zoom in functional component

--- Book_To_Comics_Client/components/functional.py
+++ Book_To_Comics_Client/components/functional.py
@@ -31,3 +31,38 @@ def error_board(service: str = "connect") -> rx.Component:
             ),
         )
     )
+
+
+def zoom_message_board() -> rx.Component:
+    # drawer component : display a message in the top , "Copied!"
+    return rx.alert_dialog(
+        rx.alert_dialog_overlay(
+            rx.alert_dialog_content(
+                rx.image(
+                    src=State.zoom_image,
+                    on_click=lambda: State.toggle_zoom(""),
+                ),
+            ),
+        ),
+        is_open=State.is_zoomed,
+        on_overlay_click=lambda: State.toggle_zoom(""),
+    )
+
+
+def copy_message_board() -> rx.Component:
+    # zoom image component
+
+    return rx.drawer(
+        rx.drawer_content(
+            rx.center(
+                rx.drawer_header(
+                    "Copied!",
+                    color="white",
+                ),
+            ),
+            bg="rgba(0, 0, 0, 0.3)",
+        ),
+        is_open=State.show_copy_in_top,
+        # placement prop to position drawer at top
+        placement="top",
+    )

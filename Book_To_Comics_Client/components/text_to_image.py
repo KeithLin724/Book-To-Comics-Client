bc6c68ace8c462b225ddb6ebfa921353dbf1bc16
KeithLin724 <keithlin724@gmail.com>
2023-12-01 21:02:58 +0800

    remove some useless comment

--- Book_To_Comics_Client/components/text_to_image.py
+++ Book_To_Comics_Client/components/text_to_image.py
@@ -31,19 +31,3 @@ def input_box() -> rx.Component:
         shadow="lg",
         border_radius="lg",
     )
-
-
-# def zoom_message_board() -> rx.Component:
-#     # drawer component : display a message in the top , "Copied!"
-#     return rx.alert_dialog(
-#         rx.alert_dialog_overlay(
-#             rx.alert_dialog_content(
-#                 rx.image(
-#                     src=State.zoom_image,
-#                     on_click=lambda: State.toggle_zoom(""),
-#                 ),
-#             ),
-#         ),
-#         is_open=State.is_zoomed,
-#         on_overlay_click=lambda: State.toggle_zoom(""),
-#     )
